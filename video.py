2ec9068e865e5afff74025e88d6e81ec3e8aa087
Bibek Pradhan <134042662+ADD2LIST@users.noreply.github.com>
2023-05-28 07:52:00 +0530

    Update video.py

--- video.py
+++ video.py
@@ -1,44 +1,54 @@
-import streamlit as st
+ import streamlit as st
 
-import requests
+import instaloader
 
-def download_video(url):
+def download_instagram_video(url):
 
-    # Send a request to get the video content
+    # Create an instance of Instaloader
 
-    response = requests.get(url, stream=True)
+    loader = instaloader.Instaloader()
 
-    # Extract the filename from the URL
+    # Download the video
 
-    filename = url.split("/")[-1]
+    try:
 
-    # Open a file for writing
+        loader.download_video(url, filename="downloaded_video")
 
-    with open(filename, "wb") as file:
+        st.success("Video downloaded successfully!")
 
-        # Iterate over the response content in chunks
+    except Exception as e:
 
-        for chunk in response.iter_content(chunk_size=1024):
+        st.error(f"Failed to download the video. Error: {str(e)}")
 
-            if chunk:
+def download_instagram_reel(url):
 
-                # Write the chunk to the file
+    # Create an instance of Instaloader
 
-                file.write(chunk)
+    loader = instaloader.Instaloader()
 
-    return filename
+    # Download the reel
+
+    try:
+
+        loader.download_reels(url, filename="downloaded_reel")
+
+        st.success("Reel downloaded successfully!")
+
+    except Exception as e:
+
+        st.error(f"Failed to download the reel. Error: {str(e)}")
 
 # Streamlit app code
 
-st.title("Video Downloader")
+st.title("Instagram Video and Reel Downloader")
 
 # Input URL
 
-url = st.text_input("Enter the video URL")
+url = st.text_input("Enter the Instagram video or reel URL")
 
 # Download button
 
-if st.button("Download"):
+if st.button("Download Video"):
 
     # Check if URL is provided
 
@@ -46,59 +56,25 @@ if st.button("Download"):
 
         # Download the video
 
-        try:
-
-            filename = download_video(url)
-
-            st.success("Video downloaded successfully!")
-
-            # Create a temporary download link
-
-            with open(filename, "rb") as file:
-
-                # Read the file contents
-
-                video_bytes = file.read()
-
-            # Generate a unique filename for download
-
-            download_filename = f"{filename}"
-
-            # Determine the MIME type based on the file extension
-
-            mime_type = "video/mp4"  # Default to MP4
-
-            if filename.endswith(".webm"):
-
-                mime_type = "video/webm"
-
-            elif filename.endswith(".avi"):
-
-                mime_type = "video/x-msvideo"
-
-            # Provide a download link with the specified MIME type
-
-            st.download_button(
-
-                label="Click here to download the video",
-
-                data=video_bytes,
-
-                file_name=download_filename,
-
-                mime=mime_type,
-
-            )
-
-        except Exception as e:
-
-            st.error(f"Failed to download the video. Error: {str(e)}")
+        download_instagram_video(url)
 
     else:
 
-        st.warning("Please enter a valid video URL.")
+        st.warning("Please enter a valid Instagram video URL")
+
+if st.button("Download Reel"):
+
+    # Check if URL is provided
+
+    if url:
+
+        # Download the reel
+
+        download_instagram_reel(url)
+
+    else:
+
+        st.warning("Please enter a valid Instagram reel URL")
 
 
-    
-    
-      
+              
